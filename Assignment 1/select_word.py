b0c653140272a61e6b880dab1c9cd7f43851467e
SIWAKORN <arsurarizen@gmail.com>
2018-01-21 21:46:27 +0700

    select word

---
+++ Assignment 1/select_word.py
@@ -0,0 +1,33 @@
+# word list from https://github.com/dwyl/english-words
+
+# while read word list file
+#   if any line have n letter word and n_word list < 100
+#       append word to n_word list
+# write n_word list to textfile
+
+def read_lines(filename, n_letter, max_word):
+    lines = []
+    with open(filename, 'r') as infile:
+        for line in infile:
+            if len(line) == n_letter and len(lines) < max_word:
+                lines.append(line)
+
+    return lines
+
+def write_lines(filename, word):
+    outfile = open(filename, 'a')
+    outfile.write("".join(word))
+    outfile.close()
+
+def main():
+    n_letter = [] 
+    for i in range(3, 6):
+        n_letter.append(read_lines("words_alpha.txt", i, 100))
+    
+    for item in n_letter:
+        write_lines("300_words.txt", item)
+
+
+
+if __name__ == "__main__":
+    main()
